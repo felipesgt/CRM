a55956aadf2caefd445afee241f1b70911cf31ac
felipe <felipesgoncalvess@gmail.com>
2021-02-11 10:40:38 -0300

    add: base repo

--- back-end/src/Api.Domain/Entities/CustomerEntity.cs
+++ back-end/src/Api.Domain/Entities/CustomerEntity.cs
@@ -1,6 +1,19 @@
-﻿namespace Domain
+﻿using Api.Domain.Entities;
+
+namespace Domain
 {
-    public class CustomerEntity
-    {
-    }
+    public class CustomerEntity : BaseEntity { 
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string CPF { get; set; }
+            public string Date { get; set; }
+            public string Phone { get; set; }
+            public string ZipCode { get; set; }
+            public string Number { get; set; }
+            public string Neighborhood { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+        }
+  
+
 }
