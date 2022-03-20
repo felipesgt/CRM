7f5802205245dbdbeb61e3f30e255dc3149f7e61
felipesgt <felipesgoncalvess@gmail.com>
2022-03-20 17:48:43 -0300

    Add: Lead entity

---
+++ back-end/Domain/Leads/Lead.cs
@@ -0,0 +1,32 @@
+﻿using Api.Domain.Entities;
+using Domain.Leads.Dtos;
+
+namespace Domain
+{
+    [System.ComponentModel.DataAnnotations.Schema.Table("Leads")]
+    public class Lead : BaseEntity
+    {
+        public Lead()
+        {
+        }
+
+        public Lead(LeadInput input)
+        {
+            Name = input.Name;
+            Email = input.Email;
+            Address = input.Address;
+        }
+        
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public void Update(LeadInput input)
+        {
+            Name = input.Name;
+            Email = input.Email;
+            Address = input.Address;
+        }
+    }
+}
---
+++ back-end/Infrastructure/Migrations/20220320203422_Inital_Migration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Inital_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Leads",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leads", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leads");
+        }
+    }
+}
